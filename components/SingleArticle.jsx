92cb705dfc5e879bf02aee1b4586a36926fcc760
HarryW217 <harrywalker217@gmail.com>
2023-10-24 14:03:08 +0100

    Can now view a list of comments associated with an article

--- components/SingleArticle.jsx
+++ components/SingleArticle.jsx
@@ -1,9 +1,11 @@
 import { useParams, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { CommentList } from "./CommentList";
 
 export const SingleArticle = () => {
   const [article, setArticle] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const { article_id } = useParams();
 
@@ -16,15 +18,20 @@ export const SingleArticle = () => {
   };
 
   useEffect(() => {
+    setIsLoading(true);
     getArticle(article_id)
       .then((article) => {
         setArticle(article);
+        setIsLoading(false);
         setError(null);
       })
       .catch((err) => {
         setError(err);
+        setIsLoading(false);
       });
   }, [article_id]);
+
+  if (isLoading) return <p>Loading...</p>;
 
   if (error) return <p>{error.msg}</p>;
 
@@ -33,12 +40,16 @@ export const SingleArticle = () => {
       <article className="single-article">
         <h2>{article.title}</h2>
         <img src={article.article_img_url}></img>
-        <h3>Author: {article.author} | Published:{article.created_at}</h3>
+        <h3>
+          Author: {article.author} | Published: {article.created_at}
+        </h3>
         <h3>Article Votes: {article.votes}</h3>
         <p className="article-body">{article.body}</p>
-        <Link className="back-home-link" to="/">Click here to go back home</Link>
+        <Link className="back-home-link" to="/">
+          Click here to go back home
+        </Link>
       </article>
-
+      <CommentList></CommentList>
     </div>
   );
 };
