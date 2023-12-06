cb7d02e8565b6d7d7614069e0a3e6a142f6937f0
HarryW217 <harrywalker217@gmail.com>
2023-12-06 15:22:32 +0000

    Creating more flexible layouts by altering CSS, meaning the app is now more suited to mobile as well as desktop views!

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-// import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import { ArticleList } from "../components/ArticleList";
 import { SingleArticle } from "../components/SingleArticle";
 import { CommentCard } from "../components/CommentCard";
+import { Helmet } from "react-helmet";
 
 const App = () => {
   return (
     <div className="App">
+      <Helmet>
+        <title>NC NEWS</title>
+      </Helmet>
       <h1 className="website-header">NC NEWS</h1>
       <Routes>
-        <Route path="/" element={<ArticleList/>}/>
+        <Route path="/" element={<ArticleList />} />
         <Route path="/articles/:article_id" element={<SingleArticle />} />
-        <Route path="/comment/:comment_id" element={ <CommentCard/>} />
+        <Route path="/comment/:comment_id" element={<CommentCard />} />
       </Routes>
       <footer>Developed by Harry Walker. 2023.</footer>
     </div>
-
   );
 };
 
